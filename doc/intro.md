cccdc2f8faaa2e10e68087bcf5a3d91a3817ade5
Deon Moolman <cmdrdats@gmail.com>
2012-08-31 12:06:31 +0200

    Initial commit of ns-reloader

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ns-reloader
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject ns-reloader "0.1.0-SNAPSHOT"
+  :description "Functions to monitor and reload changed namespaces automatically"
+  :url "http://www.github.com/yuppiechef/ns-reloader"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [ns-tracker "0.1.1"]])
